4afdaca3f78739db72fb4fe5b8e7a2687a4c0ffb
kulmurzaeva84 <kjika84@gmail.com>
2020-10-31 11:42:51 -0500

    no trick or treat

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -4,4 +4,7 @@ public class HalloweenTest {
 // are we there yet? Hello Hello
 // we are just making some changes
 
+    public static void main(String[] args) {
+        System.out.println("No trick or treat this year");
+    }
 }
